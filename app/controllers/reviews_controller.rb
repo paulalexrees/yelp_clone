ea50751ed52661a714276ca25e77ad5a2e702259
Paul <paulalexrees@gmail.com>
2016-04-12 20:44:23 +0100

    Destroying reviews of deleted restaurants

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,7 +8,7 @@ class ReviewsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = @restaurant.reviews.create(review_params)
-    redirect_to '/restaurants'
+    redirect_to restaurants_path
   end
 
   def review_params
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -3,15 +3,30 @@ require 'rails_helper'
 feature 'reviewing' do
   let!(:kfc) { Restaurant.create name: 'KFC' }
 
-  scenario 'allows users to leave a review using a form' do
-    visit '/restaurants'
-    click_link 'Review KFC'
-    fill_in 'Thoughts', with: "so so"
-    select '3', from: 'Rating'
-    click_button 'Leave Review'
+  context 'writing a review' do
+    scenario 'allows users to leave a review using a form' do
+      visit '/restaurants'
+      click_link 'Review KFC'
+      fill_in 'Thoughts', with: "so so"
+      select '3', from: 'Rating'
+      click_button 'Leave Review'
 
-    expect(current_path).to eq '/restaurants'
-    expect(page).to have_content('so so')
+      expect(current_path).to eq '/restaurants'
+      expect(page).to have_content('so so')
+    end
+  end
+
+  context 'review destruction' do
+    scenario 'deleting a restaurant deletes its reviews' do
+      visit '/restaurants'
+      click_link 'Review KFC'
+      fill_in 'Thoughts', with: "so so"
+      select '3', from: 'Rating'
+      click_button 'Leave Review'
+      click_link 'Delete KFC'
+      expect(kfc.reviews.empty?).to eq true
+    end
+
   end
 
 end
